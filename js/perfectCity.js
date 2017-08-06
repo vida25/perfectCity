c1973dacd063882ff97d31172ab46eecb13fc518
vida25 <olepolkovodets@gmail.com>
2017-08-06 10:27:28 +0300

    third commit

--- js/perfectCity.js
+++ js/perfectCity.js
@@ -8,43 +8,61 @@
 
       // calculates the length of shortest way;
     function perfectCity(departure, destination) {
-      let result = 0;
-      if (departure[i] < destination[i]) {
-        let t = departure;
-        departure = destination;
-        destination = t;
+      if (departure[0] == destination[0] && departure[1] == destination[1] ) {
+        return 0;
       }
+      let turn = true;
+      let result = 0;
       for (var i = 0; i < 2; i++) {
+          if (departure[i] < destination[i]) {
+            let t = departure;
+            departure = destination;
+            destination = t;
+          }
           let d1;
           let d2;
           let d12;
         if (Math.floor(departure[i]) === Math.floor(destination[i])) {
-          d1 = Math.ceil(departure[i]) - departure[i];
-          d2 = Math.ceil(destination[i]) - destination[i];
-          d12 = d1 + d2;
-          if (d12 > 1) {
-            d12 = 2 - d12;
+          if (Math.floor(departure[+turn]) === Math.floor(destination[+turn])) {
+            d12 = Math.abs(destination[i] - departure[i]);
+          } else {
+            d1 = Math.ceil(departure[i]) - departure[i];
+            d2 = Math.ceil(destination[i]) - destination[i];
+            d12 = d1 + d2;
+            if (d12 !== 0 && (d1 === 0 || d2 === 0)) {
+              d12 = 1 - d12;
+            } else if (d12 > 1)  {
+              d12 = 2 - d12;
+            }
           }
         } else {
-          d1 = Math.ceil(departure[i]) - departure[i];
-          d2 = destination[i] - Math.floor(destination[i]);
-          d12 = d1 + d2;
+          d12 = Math.abs(destination[i] - departure[i]);
         }
-
-        let d = Math.abs(Math.floor(destination[i]) - Math.floor(departure[i]));
-        result += (d + d12);
+        result += d12;
+        turn = !turn;
       }
       return result;
     }
 
     function getCoordinates(elem) {
-      return elem.value.split(/,+|:+|;+|\s+/ig).map( a => +a );
+      return elem.value.split(/,+|:+|;+|\s+/ig).map( a => Number(a) );
+    }
+
+    function checkCoordinates( ...arg ) {
+      return arg.every( (coordinates) => {
+        return coordinates.some( val => Number.isInteger(val) );
+      });
     }
 
 
   calculateBtn.addEventListener('click', () => {
       let departure = getCoordinates(departureInput);
       let destination = getCoordinates(destinationInput);
+      if ( !checkCoordinates(departure, destination) ) {
+        alert("At least one of inputed values in each field should be of type integer. Please check the coordinates.");
+        resultOutput.innerHTML = "";
+        return;
+      };
       let result = perfectCity(departure, destination);
       resultOutput.innerHTML = `<strong>The length of the shortest route = ${result.toFixed(2)}</strong>`;
   }, false);
